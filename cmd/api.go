9e151d6227cd96c49571346c1ada531480868bf5
Yov00 <yovelincho@gmail.com>
2026-09-13 20:33:28 +0300

    init

---
+++ cmd/api.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+	// logger
+	// db driver
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	conStr string
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// Middleware
+	r.Use(middleware.RequestID) // good for rate limiting
+	r.Use(middleware.RealIP)    // again for rate limiting and analytics/tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good!"))
+	})
+
+	// http.ListenAndServe(app.config.addr)
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+	log.Printf("Server has started at addr %s", app.config.addr)
+	return srv.ListenAndServe()
+}
